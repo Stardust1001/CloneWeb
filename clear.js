e646f74fa3c9ef5bba62b535521802749629dc61
Stardust <2368354416@qq.com>
2026-03-21 22:51:56 +0800

    add

--- clear.js
+++ clear.js
@@ -4,11 +4,26 @@ import { promises } from '@wp1001/js'
 import { config } from './common.js'
 
 const files = await fsUtils.listAll(config.dirname)
+const items = []
+await promises.schedule(async i => {
+  const isDir = await fsUtils.isDir(files[i])
+  items.push([files[i], !!isDir])
+}, files.length, 20)
+items.sort((a, b) => a[1] - b[1])
+
 let numClears = 0
 await promises.schedule(async i => {
-  const stat = await fsUtils.stat(files[i])
-  if (!stat.size) {
-    await fsUtils.remove(files[i])
-    console.log(`已删除 ${++numClears} 个空文件: ${files[i]}`)
+  const [file, isDir] = items[i]
+  let isEmpty = false
+  if (isDir) {
+    const subs = await fsUtils.listDir(file)
+    isEmpty = !subs.length
+  } else {
+    const stat = await fsUtils.stat(file)
+    isEmpty = !stat.size
   }
-}, files.length, 20)
+  if (isEmpty) {
+    await fsUtils.remove(file)
+    console.log(`已删除 ${++numClears} 个空文件/文件夹: ${file}`)
+  }
+}, items.length, 20)
